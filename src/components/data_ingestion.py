4581e403143fc7f8a6ab347b5bedf34e18815069
Evan Arnanda <evanarnanda@gmail.com>
2023-08-01 16:17:11 +0700

    Added Script for Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,13 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
+from dataclasses import dataclass
+
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTransformation
 
 
 @dataclass
@@ -25,20 +27,31 @@ class DataIngestion:
             df = pd.read_csv('notebook/data/StudentsPerformance.csv')
             logging.info('Read the datasets as DataFrame')
 
-            os.makedirs(os.path.dirname(
-                self.ingestion_config.train_data_path), exist_ok=True)
+            df.columns = [i.replace(' ', '_')
+                          for i in df.columns]
+            logging.info('Replacing white space to "_" in column name')
 
-            df.to_csv(self.ingestion_config.raw_data_path,
-                      index=False, header=True)
+            os.makedirs(
+                os.path.dirname(
+                    self.ingestion_config.train_data_path
+                ), exist_ok=True
+            )
+
+            df.to_csv(
+                self.ingestion_config.raw_data_path, index=False, header=True
+            )
 
             logging.info('Train test split initiated')
             train_set, test_set = train_test_split(
-                df, test_size=0.2, random_state=42)
+                df, test_size=0.2, random_state=42
+            )
 
             train_set.to_csv(
-                self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path,
-                            index=False, header=True)
+                self.ingestion_config.train_data_path, index=False, header=True
+            )
+            test_set.to_csv(
+                self.ingestion_config.test_data_path, index=False, header=True
+            )
 
             logging.info('Ingestion of the data is completed')
             return (
@@ -51,4 +64,7 @@ class DataIngestion:
 
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
