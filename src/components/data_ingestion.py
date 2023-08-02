69de73e96305c95ffaf9b9f1a24d33edcb0935be
Evan Arnanda <evanarnanda@gmail.com>
2023-08-02 12:31:04 +0700

    Model trainer and Added new util

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from src.exception import CustomException
 from src.logger import logging
+
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -67,4 +69,10 @@ if __name__ == '__main__':
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
+        train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    logging.info(modeltrainer.initiate_model_trainer(
+        train_array=train_arr, test_array=test_arr
+    ))
