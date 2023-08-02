fedb4e1a04ef0a0444a04c6d8cf5a3eec8734d73
Evan Arnanda <evanarnanda@gmail.com>
2023-08-02 13:04:52 +0700

    Added script for hyperparameter

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -24,13 +24,23 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            try:
+                param = params[list(models.keys())[
+                    list(models.values()).index(model)
+                ]]
+            except:
+                param = {}
 
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
